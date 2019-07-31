986d7a7e6b6d143969febd55eca4fa5a5b8224de
Alan Mitchell <tabb99@gmail.com>
2019-07-31 09:15:12 -0800

    Restructured utility splitting functions.

--- bmondata/util.py
+++ bmondata/util.py
@@ -1,9 +1,17 @@
 """Utility functions for the bmondata package.
 """
 
-def cs_to_list(cs_str):
+def csnl_to_list(csnl_str):
     """Converts a comma-separated and new-line separated string into
-    a list.  Does not return any zero-length strings.
+    a list, stripping whitespace from both ends of each item.  Does not
+    return any zero-length strings.
     """
-    s = cs_str.replace('\n', ',')
+    s = csnl_str.replace('\n', ',')
+    return [it.strip() for it in s.split(',') if it.strip()]
+
+def split_strip(s, delim=','):
+    """Converts a delimited string into a list, stripping whitespace 
+    from both ends of each item.  Does not return any zero-length strings.
+    'delim' is the delimiter.
+    """
     return [it.strip() for it in s.split(',') if it.strip()]
