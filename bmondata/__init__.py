890c963ad74f6b74a75e5eae644229662f17d147
Alan Mitchell <tabb99@gmail.com>
2019-11-20 19:35:21 -0900

    Added schedule module for interpreting Building Schedules.

--- bmondata/__init__.py
+++ bmondata/__init__.py
@@ -1,7 +1,12 @@
-__version__ = '1.0.2'
+__version__ = '1.0.3'
+# Version info
+# 1.0.3:  added schedule module
 
 # Classes and methods that are in the top-level namespace
 from .server import Server
 
 # Make all utility functions top-level
 from .util import *
+
+# Import Schedule class into this namespace
+from .schedule import Schedule
