07b14ba20a9c8d678a52794e9d66fc7f595166ba
Alan Mitchell <tabb99@gmail.com>
2019-11-20 20:09:07 -0900

    Increased version number.

--- bmondata/__init__.py
+++ bmondata/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 # Version info
-# 1.0.3:  added schedule module
+# 1.0.4: Added schedule module
+# 1.0.3: Bad release, had to delete.
 
 # Classes and methods that are in the top-level namespace
 from .server import Server
